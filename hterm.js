c6d0280c7182101e6862cd2cee005a96c9a40c7f
Jean-Francois Turcot <jf.turcot@gmail.com>
2012-09-14 02:23:00 -0400

    Add my hterm settings for Google Chrome

---
+++ hterm.js
@@ -0,0 +1,15 @@
+// Disable bold
+term_.prefs_.set('enable-bold', false);
+
+// Solarized Dark
+term_.prefs_.set('background-color', "#002B36");
+term_.prefs_.set('foreground-color', "#839496");
+
+// Solarized Light
+// term_.prefs_.set('background-color', "#fdf6e3");
+// term_.prefs_.set('foreground-color', "#657b83");
+
+// Font settings
+term_.prefs_.set('font-family', 'Monaco');
+term_.prefs_.set('font-size', 16);
+term_.prefs_.set('font-smoothing', 'subpixel-antialiased');
