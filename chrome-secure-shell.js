56f048f9d8c33741402276453863604263eab6f6
Jean-Francois Turcot <jf.turcot@gmail.com>
2013-09-15 23:25:10 -0400

    Rename hterm.js to chrome-secure-shell.js and add Solarized colors

--- chrome-secure-shell.js
+++ chrome-secure-shell.js
@@ -4,6 +4,24 @@ term_.prefs_.set('enable-bold', false);
 // Solarized Dark
 term_.prefs_.set('background-color', "#002B36");
 term_.prefs_.set('foreground-color', "#839496");
+term_.prefs_.set('color-palette-overrides', [
+    '#073642', 
+    '#dc322f', 
+    '#859900', 
+    '#b58900', 
+    '#268bd2', 
+    '#d33682', 
+    '#2aa198', 
+    '#eee8d5', 
+    '#002b36', 
+    '#cb4b16', 
+    '#586e75', 
+    '#657b83', 
+    '#839496', 
+    '#6c71c4', 
+    '#93a1a1', 
+    '#fdf6e3'
+  ]);
 
 // Solarized Light
 // term_.prefs_.set('background-color', "#fdf6e3");
